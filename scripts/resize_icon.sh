aa008ace33195e8f1e592cff9c6d4a6786fbf10e
Augustin de Latrollière <augustin.dlt@gmail.com>
2020-07-13 17:18:47 +0200

    feat(extension): Add an icon

---
+++ scripts/resize_icon.sh
@@ -0,0 +1,4 @@
+# Need ImageMagick installed
+convert -resize 16x16 "icon/icon.png" "icon/icon16.png"
+convert -resize 48x48 "icon/icon.png" "icon/icon48.png"
+convert -resize 128x128 "icon/icon.png" "icon/icon128.png"
